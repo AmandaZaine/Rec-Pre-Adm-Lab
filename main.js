fa1047915af10abbfcceb9746efb8aa90ab59f5c
AmandaZaine <59513136+AmandaZaine@users.noreply.github.com>
2020-03-27 00:24:22 -0300

    Quarto commit main.js
    Quarto commit main.js

--- main.js
+++ main.js
@@ -27,21 +27,23 @@ function goThemeTwo() {
 
 function eventAnswers() {
     document.getElementById("sendAnswers").addEventListener("click",showAnswers);
+    document.getElementById("gameOneAgain").addEventListener("click",goThemeOne);
+    document.getElementById("gameTwoAgain").addEventListener("click",goThemeTwo); 
+    document.getElementById("closeGame").addEventListener("click",closeAllGame); 
+}
+
+function closeAllGame(){
+    return window.close();   
 }
 
 
 function showAnswers(){
     document.getElementById("allQuestions").style.display = "none";
 
-    //document.getElementById("showFinalAnswer").style.display = "block";
-    /*
-    .showFinalAnswer{
-    display: none;
-    }
-    */
+    document.getElementById("showFinalAnswer").style.display = "block";
 
-
-
+    document.getElementById("playAgain").style.visibility = "visible";  
+    
     if (document.getElementById("right1a").checked == true) {
         document.getElementById("rightAnswers").innerHTML += "<p>Você acertou a pergunta 1</p>";
     }else{
@@ -58,64 +60,6 @@ function showAnswers(){
         document.getElementById("rightAnswers").innerHTML += "<p>Você acertou a pergunta 3</p>";
     }else{
         document.getElementById("wrongAnswers").innerHTML += "<p>Você errou a pergunta 3</p>";
-    }
-}
-
-
-
-
-
-
-
-/*  
-    
-    
-    }
-     
-    let terceiraPergunta = parseInt(prompt("Terceira Pergunda: \n Quanto é 9*9 ? \n Digite número correspondente a sua escolha: \n 1. 72 \n 2. 81 \n 3. 35"));
-    if (terceiraPergunta === 2){
-        let respostaCorreta3 = document.getElementById("resposta-correta-1");
-        respostaCorreta3.innerHTML += "<p>Você acertou a questão 3</p>";
-        
-    } else {
-         let respostaErrada3 = document.getElementById("resposta-errada-1");
-        respostaErrada3.innerHTML += "<p>Você errou a questão 3</p>";
-    }
-}
-else {
-    let questao1 = parseInt(prompt("Primeira Pergunda: \n O que é Header? \n Digite número correspondente a sua escolha: \n 1. Cabeçalho \n 2. Corpo \n 3. Comida "));
-    if (questao1 === 1){
-        let respostaCorreta1 = document.getElementById("resposta-correta-1");
-        respostaCorreta1.innerHTML += "<p>Você acertou a questão 1</p>";
-        
-    } else {
-         let respostaErrada1 = document.getElementById("resposta-errada-1");
-        respostaErrada1.innerHTML += "<p>Você errou a questão 1</p>";
-    }
-
-    let questao2 = parseInt(prompt("Segunda Pergunda: \n O que é uma div? \n Digite número correspondente a sua escolha: \n 1. Uma tag de divisão \n 2. Discussão \n 3. Dividendo"));
-    if (questao2 === 1){
-        let respostaCorreta2 = document.getElementById("resposta-correta-1");
-        respostaCorreta2.innerHTML +=  "<p>Você acertou a questão 2</p>";
-        
-    } else {
-         let respostaErrada2 = document.getElementById("resposta-errada-1");
-        respostaErrada2.innerHTML += "<p>Você errou a questão 2</p>";
-    }
-     
-    let questao3 = parseInt(prompt("Terceira Pergunda: \n Pra que serve \\n ? \n Digite número correspondente a sua escolha: \n 1. Pular linha \n 2. Pular resposta \n 3. Parar"));
-    if (questao3 === 1){
-        let respostaCorreta3 = document.getElementById("resposta-correta-1");
-        respostaCorreta3.innerHTML += "<p>Você acertou a questão 3</p>";
-        
-    } else {
-         let respostaErrada3 = document.getElementById("resposta-errada-1");
-        respostaErrada3.innerHTML += "<p>Você errou a questão 3</p>";
-    }
-
-
-
+    }  
 
 }
-
-*/
